ab6d26fafd79e5b47c6784577cdd97bc79fa3e0c
erith <erith9@gmail.com>
2018-05-28 22:33:45 +0900

    initialize

---
+++ WpfApp1/BinCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVV_SAMPLE01
+{
+    public class ScoreCollection : ObservableCollection<Score>
+    {
+        public ScoreCollection()
+        {
+            Add(new Score() { SUBJECT = "국어", SCORE = 95 });
+            Add(new Score() { SUBJECT = "영어", SCORE = 55 });
+            Add(new Score() { SUBJECT = "수학", SCORE = 65 });
+        }
+    }
+
+    public class Score
+    {
+        public string SUBJECT { get; set; }
+        public int SCORE { get; set; }
+    }
+
+
+}
---
+++ WpfApp1/ItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVV_SAMPLE01
+{
+    public class ItemViewModel
+    {
+        private readonly ScoreCollection items;
+
+        public ItemViewModel()
+        {
+            this.items = new ScoreCollection();
+        }
+
+        public ScoreCollection Items
+        {
+            get { return this.items; }
+        }
+    }
+}
